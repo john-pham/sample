452c11e6af61779fcf21d43e587911539b5beefd
quangle <lt.quang.it@gmail.com>
2018-05-30 23:05:00 +0700

    validate class

--- src/ebrain.admin.web/ClientApp/app/components/classes/classes.component.ts
+++ src/ebrain.admin.web/ClientApp/app/components/classes/classes.component.ts
@@ -1,4 +1,4 @@
-// ======================================
+﻿// ======================================
 // Author: Ebrain Team
 // Email:  johnpham@ymail.com
 // Copyright (c) 2017 supperbrain.visualstudio.com
@@ -182,44 +182,24 @@ export class ClassesComponent implements OnInit, OnDestroy {
     private getFromServer(isReset: boolean) {
         this.loadingIndicator = true;
         
-        var disp = this.materialService.getMaterialLearn(this.filterName, this.filterValue, 0, 0).subscribe(
+        this.materialService.getMaterialLearn(this.filterName, this.filterValue, 0, 0).subscribe(
             list => this.onDataLoadMaterialSuccessful(list),
-            error => this.onDataLoadFailed(error),
-            () => {
-                disp.unsubscribe();
-                setTimeout(() => { this.loadingIndicator = false; }, 1500);
-            });
+            error => this.onDataLoadFailed(error));
 
         this.classStatusService.getAll().subscribe(
             list => this.onDataLoadClassStatusSuccessful(list),
-            error => this.onDataLoadFailed(error),
-            () => {
-                disp.unsubscribe();
-                setTimeout(() => { this.loadingIndicator = false; }, 1500);
-            });
+            error => this.onDataLoadFailed(error));
         this.supplierService.search("", "", 4, 0, 0).subscribe(
             list => this.onDataLoadSupplierSuccessful(list),
-            error => this.onDataLoadFailed(error),
-            () => {
-                disp.unsubscribe();
-                setTimeout(() => { this.loadingIndicator = false; }, 1500);
-            });
+            error => this.onDataLoadFailed(error));
 
         this.todayService.getAll().subscribe(
             list => this.onDataLoadTodaySuccessful(list),
-            error => this.onDataLoadFailed(error),
-            () => {
-                disp.unsubscribe();
-                setTimeout(() => { this.loadingIndicator = false; }, 1500);
-            });
+            error => this.onDataLoadFailed(error));
 
         this.roomService.search("", "", 0, 0).subscribe(
             list => this.onDataLoadRoomSuccessful(list),
-            error => this.onDataLoadFailed(error),
-            () => {
-                disp.unsubscribe();
-                setTimeout(() => { this.loadingIndicator = false; }, 1500);
-            });
+            error => this.onDataLoadFailed(error));
 
         this.studentService.search("", "").subscribe(
             list => {
@@ -227,18 +207,10 @@ export class ClassesComponent implements OnInit, OnDestroy {
                 //get default
                 this.getDefault("", isReset);
             },
-            error => this.onDataLoadFailed(error),
-            () => {
-                disp.unsubscribe();
-                setTimeout(() => { this.loadingIndicator = false; }, 1500);
-            });
+            error => this.onDataLoadFailed(error));
         this.shiftService.search("", "", 0, 0).subscribe(
             list => this.onDataLoadShiftSuccessful(list),
-            error => this.onDataLoadFailed(error),
-            () => {
-                disp.unsubscribe();
-                setTimeout(() => { this.loadingIndicator = false; }, 1500);
-            });
+            error => this.onDataLoadFailed(error));
         //get default
         this.getDefault("", isReset);
 
@@ -270,6 +242,7 @@ export class ClassesComponent implements OnInit, OnDestroy {
             this.pointer.shiftId = list[0].id;
         }
         this.shifts = list;
+        this.loadingIndicator = false;
     }
 
     private onDataLoadStudentSuccessful(list: Student[]) {
@@ -277,6 +250,7 @@ export class ClassesComponent implements OnInit, OnDestroy {
             this.pointer.studentId = list[0].id;
         }
         this.students = list;
+        this.loadingIndicator = false;
     }
 
     private onDataLoadRoomSuccessful(resulted: Results<Room>) {
@@ -285,6 +259,7 @@ export class ClassesComponent implements OnInit, OnDestroy {
             this.pointer.roomId = list[0].id;
         }
         this.rooms = list;
+        this.loadingIndicator = false;
     }
 
     private onDataLoadTodaySuccessful(list: Today[]) {
@@ -292,6 +267,7 @@ export class ClassesComponent implements OnInit, OnDestroy {
             this.pointer.todayId = list[0].id;
         }
         this.todays = list;
+        this.loadingIndicator = false;
     }
 
     private onDataLoadSupplierSuccessful(resulted: Results<Supplier>) {
@@ -301,6 +277,7 @@ export class ClassesComponent implements OnInit, OnDestroy {
             this.pointer.teacherTodayId = list[0].id;
         }
         this.suppliers = list;
+        this.loadingIndicator = false;
     }
 
     private onDataLoadClassStatusSuccessful(list: ClassStatus[]) {
@@ -309,7 +286,7 @@ export class ClassesComponent implements OnInit, OnDestroy {
         }
         this.status = list;
         this.alertService.stopLoadingMessage();
-
+        this.loadingIndicator = false;
     }
 
     private onDataLoadMaterialSuccessful(resulted: Results<MaterialLearn>) {
@@ -319,19 +296,19 @@ export class ClassesComponent implements OnInit, OnDestroy {
         }
         this.materials = list;
         this.alertService.stopLoadingMessage();
-
+        this.loadingIndicator = false;
     }
     
     private onDataLoadFailed(error: any) {
         this.alertService.stopLoadingMessage();
         this.alertService.showStickyMessage("Load Error", `Unable to retrieve user data from the server.\r\nErrors: "${Utilities.getHttpResponseMessage(error)}"`,
             MessageSeverity.error, error);
-
+        this.loadingIndicator = false;
     }
 
     private save() {
         this.alertService.startLoadingMessage("Saving changes...");
-
+        this.loadingIndicator = false;
         this.pointer.times = this.rowTimes;
         this.pointer.students = this.rowStudents;
         this.localService.save(this.pointer).subscribe(value => this.saveSuccessHelper(value), error => this.saveFailedHelper(error));
@@ -397,6 +374,7 @@ export class ClassesComponent implements OnInit, OnDestroy {
 
         if (this.changesSavedCallback)
             this.changesSavedCallback();
+        this.loadingIndicator = false;
     }
 
     private saveFailedHelper(error: any) {
@@ -409,65 +387,87 @@ export class ClassesComponent implements OnInit, OnDestroy {
     }
 
     private addClassTime(id: string) {
+        let err = "";
         var item = this.pointer;
-        var itemNew = new ClassTime();
-        itemNew.onTodayId = item.todayId;
-        itemNew.startTime = item.startTime;
-        itemNew.endTime = item.endTime;
-        itemNew.roomId = item.roomId;
-        itemNew.supplierId = item.teacherTodayId;
-        itemNew.id = id;
-        itemNew.shiftId = item.shiftId;
-        itemNew.classId = item.id;
-        var room = this.rooms.filter(x => x.id == item.roomId)[0];
-        if (room != null) {
-            itemNew.roomName = room.name;
-        }
+        if (item.todayId == null || item.todayId.length == 0) err = "Vui lòng chọn thứ";
+        else if (item.roomId == null || item.roomId.length == 0) err = "Vui lòng chọn phòng học";
+        else if (item.shiftId == null || item.shiftId.length == 0) err = "Vui lòng chọn ca học";
 
-        var today = this.todays.filter(x => x.id == item.todayId)[0];
-        if (today != null) {
-            itemNew.todayName = today.name;
+        if (err.length > 0) {
+            this.alertService.showStickyMessage("Save Error", err, MessageSeverity.error);
         }
+        else {
+            var itemNew = new ClassTime();
+            itemNew.onTodayId = item.todayId;
+            itemNew.startTime = item.startTime;
+            itemNew.endTime = item.endTime;
+            itemNew.roomId = item.roomId;
+            itemNew.supplierId = item.teacherTodayId;
+            itemNew.id = id;
+            itemNew.shiftId = item.shiftId;
+            itemNew.classId = item.id;
+            var room = this.rooms.filter(x => x.id == item.roomId)[0];
+            if (room != null) {
+                itemNew.roomName = room.name;
+            }
 
-        var sup = this.suppliers.filter(x => x.id == item.supplierId)[0];
-        if (sup != null) {
-            itemNew.supplierName = sup.name;
+            var today = this.todays.filter(x => x.id == item.todayId)[0];
+            if (today != null) {
+                itemNew.todayName = today.name;
+            }
+
+            var sup = this.suppliers.filter(x => x.id == item.supplierId)[0];
+            if (sup != null) {
+                itemNew.supplierName = sup.name;
+            }
+
+            var shift = this.shifts.filter(x => x.id == item.shiftId)[0];
+            if (shift != null) {
+                itemNew.shiftName = shift.name;
+            }
+
+            this.rowTimes.push(itemNew);
+            this.rowTimes = [...this.rowTimes]
+            this.pointer.times = this.rowTimes;
         }
-
-        var shift = this.shifts.filter(x => x.id == item.shiftId)[0];
-        if (shift != null) {
-            itemNew.shiftName = shift.name;
-        }
-
-        this.rowTimes.push(itemNew);
-        this.rowTimes = [...this.rowTimes]
-        this.pointer.times = this.rowTimes;
     }
 
     private addClassStudent(id: string) {
+        let err = "";
+
         var item = this.pointer;
-        var itemNew = new ClassStudent();
-        itemNew.studentId = item.studentId;
-        var student = this.students.filter(x => x.id == item.studentId)[0];
-        if (student != null) {
-            itemNew.fullName = student.name;
-            itemNew.address = student.address;
-        }
-
-        var mate = this.materials.filter(x => x.id == item.materialId)[0];
-        if (mate != null) {
-            itemNew.materialName = mate.name;
-        }
-
-        itemNew.startDate = item.startDate;
-        itemNew.endDate = item.endDate;
-        itemNew.materialId = item.materialId;
         
-        itemNew.classId = item.id;
-        itemNew.id = id;
-        this.rowStudents.push(itemNew);
-        this.rowStudents = [...this.rowStudents]
-        this.pointer.students = this.rowStudents;
+         if (item.studentId == null || item.studentId.length == 0) err = "Vui lòng chọn học viên";
+        else if (item.materialId == null || item.materialId.length == 0) err = "Vui lòng chọn khóa học";
+        else if (item.startDate == null) err = "Vui lòng chọn bắt đầu";
+        else if (item.endDate == null) err = "Vui lòng chọn kết thúc";
+
+        if (err.length > 0) {
+            this.alertService.showStickyMessage("Save Error", err, MessageSeverity.error);
+        } else {
+            var itemNew = new ClassStudent();
+            itemNew.studentId = item.studentId;
+            var student = this.students.filter(x => x.id == item.studentId)[0];
+            if (student != null) {
+                itemNew.fullName = student.name;
+                itemNew.address = student.address;
+            }
+
+            var mate = this.materials.filter(x => x.id == item.materialId)[0];
+            if (mate != null) {
+                itemNew.materialName = mate.name;
+            }
+
+            itemNew.startDate = item.startDate;
+            itemNew.endDate = item.endDate;
+            itemNew.materialId = item.materialId;
+
+            itemNew.classId = item.id;
+            itemNew.id = id;
+            this.rowStudents.push(itemNew);
+            this.rowStudents = [...this.rowStudents]
+            this.pointer.students = this.rowStudents;
+        }
     }
 
     deleteTime(row) {
